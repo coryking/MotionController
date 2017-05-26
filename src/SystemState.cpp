f5391347908468cf2d584d3e6544961648e5af79
Cory R. King <me@coryking.com>
2017-05-26 14:30:57 -0700

    This shit might not work....

--- src/SystemState.cpp
+++ src/SystemState.cpp
@@ -7,41 +7,58 @@
 #include "rtc.h"
 #include "Pins.h"
 
-void SystemState::StartAlarm(AlarmSettings *alarmSettings, voidFuncPtr cb) {
+using namespace std;
+
+void SystemState::StartAlarm(AlarmData *data) {
+    BEGIN_TRANSITION_MAP
+        TRANSITION_MAP_ENTRY(ST_WAIT_FOR_ALARM)   // ST_IDLE
+        TRANSITION_MAP_ENTRY(EVENT_IGNORED)   // ST_WAIT_FOR_ALARM
+        TRANSITION_MAP_ENTRY(CANNOT_HAPPEN) // ST_SHOOTING
+        TRANSITION_MAP_ENTRY(EVENT_IGNORED) // ST_WAIT_TO_FINISH_SHOOTING
+    END_TRANSITION_MAP(data)
+}
+
+void SystemState::BeginShooting(ShootingData* data) {
+    BEGIN_TRANSITION_MAP
+        TRANSITION_MAP_ENTRY(ST_SHOOTING)   // ST_IDLE
+        TRANSITION_MAP_ENTRY(ST_SHOOTING)   // ST_WAIT_FOR_ALARM
+        TRANSITION_MAP_ENTRY(EVENT_IGNORED) // ST_SHOOTING
+        TRANSITION_MAP_ENTRY(EVENT_IGNORED) // ST_WAIT_TO_FINISH_SHOOTING
+    END_TRANSITION_MAP(data)
+}
+
+STATE_DEFINE(SystemState,   Idle,   NoEventData)
+{
+    // Noop
+}
+
+STATE_DEFINE(SystemState,   Shooting,  ShootingData) {
+    data->stateMachine->start(data->settings);
+    InternalEvent(ST_WAIT_TO_FINISH_SHOOTING);
+}
+
+STATE_DEFINE(SystemState, WaitToFinishShooting, ShootingData) {
+    if(data->stateMachine->getCurrentState() == FINISHED)
+        InternalEvent(ST_IDLE);
+}
+
+STATE_DEFINE(SystemState,   WaitForAlarm, AlarmData) {
     DS3231AlarmOne alarm1(
-            alarmSettings->day,
-            alarmSettings->hour,
-            alarmSettings->minute,
-            alarmSettings->second,
+            data->day,
+            data->hour,
+            data->minute,
+            data->second,
             DS3231AlarmOneControl_HoursMinutesSecondsMatch
     );
     globalRtc.SetAlarmOne(alarm1);
 
 
-    attachInterrupt(RTC_INTERUPT_PIN, cb, FALLING);
+    attachInterrupt(RTC_INTERUPT_PIN, data->interruptServiceCallback, FALLING);
 
     globalRtc.LatchAlarmsTriggeredFlags();
 
-    this->setState(SYSTEM_WAIT_FOR_ALARM);
 }
 
-void SystemState::BeginShooting(IntervalometerSettings intervalometerSettings,  IntervalometerStateMachine* intervalometerStateMachine) {
-    intervalometerStateMachine->start(&intervalometerSettings);
-    this->setState(SYSTEM_SHOOTING);
-}
-
-void SystemState::AlarmTriggered(IntervalometerSettings intervalometerSettings, IntervalometerStateMachine * intervalometerStateMachine) {
+EXIT_DEFINE(SystemState, ExitWaitForAlarm) {
     detachInterrupt(RTC_INTERUPT_PIN);
-
-    if(this->getCurrentState() == SYSTEM_WAIT_FOR_ALARM) {
-        this->BeginShooting(intervalometerSettings, intervalometerStateMachine);
-    }
-}
-
-void SystemState::setState(SystemStates newState) {
-    this->currentState = newState;
-}
-
-SystemStates SystemState::getCurrentState() {
-    return this->currentState;
 }
--- src/SystemState.h
+++ src/SystemState.h
@@ -7,44 +7,61 @@
 
 
 #include <Arduino.h>
-#include <functional>
 
-#include "EventData.h"
+#include "StateMachine.h"
 #include "IntervalometerSettings.h"
 #include "IntervalometerStateMachine.h"
 
+
 typedef void (*voidFuncPtr)(void);
 
-typedef std::function<void(void)> fp;
-
-struct AlarmSettings : public EventData {
+struct AlarmData : public EventData {
     uint8_t day;
     uint8_t hour;
     uint8_t minute;
     uint8_t second;
+    voidFuncPtr interruptServiceCallback;
+};
+
+struct ShootingData : public EventData {
+    IntervalometerSettings *settings;
+    IntervalometerStateMachine *stateMachine;
 };
 
 
-enum SystemStates {
-    SYSTEM_IDLE,
-    SYSTEM_WAIT_FOR_ALARM,
-    SYSTEM_SHOOTING
-};
-
-class SystemState {
+class SystemState : public StateMachine {
 public:
-    SystemState() {}
+    SystemState() : StateMachine(ST_MAX_STATES) {}
 
-    SystemStates getCurrentState();
-
-    void StartAlarm(AlarmSettings*, voidFuncPtr);
-    void AlarmTriggered(IntervalometerSettings, IntervalometerStateMachine*);
-    void BeginShooting(IntervalometerSettings, IntervalometerStateMachine*);
-
+    void StartAlarm(AlarmData*);
+    void BeginShooting(ShootingData*);
+    void Poll(ShootingData*);
 private:
-    SystemStates currentState;
 
-    void setState(SystemStates newState);
+
+    // state enumeration order must match the order of state
+    // method entries in the state map
+    enum E_States {
+        ST_IDLE = 0,
+        ST_WAIT_FOR_ALARM,
+        ST_SHOOTING,
+        ST_WAIT_TO_FINISH_SHOOTING,
+        ST_MAX_STATES
+    };
+
+    STATE_DECLARE(SystemState,  Idle,   NoEventData);
+    STATE_DECLARE(SystemState,  WaitForAlarm,   AlarmData);
+    EXIT_DECLARE(SystemState, ExitWaitForAlarm);
+    STATE_DECLARE(SystemState,  Shooting,  ShootingData);
+    STATE_DECLARE(SystemState, WaitToFinishShooting, ShootingData);
+
+    // state map to define state function order
+    BEGIN_STATE_MAP_EX
+        STATE_MAP_ENTRY_EX(&Idle)
+        STATE_MAP_ENTRY_ALL_EX(&WaitForAlarm, 0, 0, &ExitWaitForAlarm)
+        STATE_MAP_ENTRY_EX(&Shooting)
+        STATE_MAP_ENTRY_EX(&WaitToFinishShooting)
+    END_STATE_MAP_EX
 
 };
 
